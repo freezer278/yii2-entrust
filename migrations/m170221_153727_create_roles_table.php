cac4d1145e83df1fc48fa50198e1c746817d12e1
Vladimir Morozov <freezer278@gmail.com>
2017-02-23 13:13:58 +0200

    added basic migration for entrust tables and models

---
+++ migrations/m170221_153727_create_roles_table.php
@@ -0,0 +1,162 @@
+<?php
+namespace vmorozov\entrust\migrations;
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `posts`.
+ */
+class m170221_153727_create_entrusts_tables extends Migration
+{
+    /**
+     * Table of User model to use it in relations.
+     * @var
+     */
+    private $usersTable;
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct()
+    {
+        parent::construct;
+        $this->usersTable = app\models\User::tableName();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        $this->db = app\models\User::getDb();
+        parent::init();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('roles', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'display_name' => $this->string(),
+            'description' => $this->string(),
+        ]);
+
+        $this->createTable('permissions', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'display_name' => $this->string(),
+            'description' => $this->string(),
+        ]);
+
+        $this->createTable('permission_role', [
+            'role_id' => $this->integer(),
+            'permission_id' => $this->integer()
+        ]);
+        // add foreign key for table `permission_role`
+        $this->addForeignKey(
+            'fk-permission_role-role_id',
+            'permission_role',
+            'role_id',
+            'roles',
+            'id',
+            'CASCADE'
+        );
+        // add foreign key for table `permission_role`
+        $this->addForeignKey(
+            'fk-permission_role-permission_id',
+            'permission_role',
+            'permission_id',
+            'permissions',
+            'id',
+            'CASCADE'
+        );
+
+        $this->createTable('role_user', [
+            'role_id' => $this->integer(),
+            'user_id' => $this->integer(),
+        ]);
+        // add foreign key for table `role_user`
+        $this->addForeignKey(
+            'fk-role_user-role_id',
+            'role_user',
+            'role_id',
+            'roles',
+            'id',
+            'CASCADE'
+        );
+        // add foreign key for table `role_user`
+        $this->addForeignKey(
+            'fk-role_user-role_id',
+            'role_user',
+            'user_id',
+            $this->usersTable,
+            'id',
+            'CASCADE'
+        );
+
+        $this->createTable('permission_user', [
+            'permission_id' => $this->integer(),
+            'user_id' => $this->integer(),
+        ]);
+        // add foreign key for table `permission_user`
+        $this->addForeignKey(
+            'fk-role_user-permission_id',
+            'permission_user',
+            'permission_id',
+            'permissions',
+            'id',
+            'CASCADE'
+        );
+        // add foreign key for table `permission_user`
+        $this->addForeignKey(
+            'fk-role_user-role_id',
+            'permission_user',
+            'user_id',
+            $this->usersTable,
+            'id',
+            'CASCADE'
+        );
+
+
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropForeignKey(
+            'fk-permission_role-role_id',
+            'permission_role'
+        );
+        $this->dropForeignKey(
+            'fk-permission_role-permission_id',
+            'permission_role'
+        );
+        $this->dropForeignKey(
+            'fk-role_user-role_id',
+            'role_user'
+        );
+        $this->dropForeignKey(
+            'fk-role_user-role_id',
+            'role_user'
+        );
+        $this->dropForeignKey(
+            'fk-role_user-permission_id',
+            'permission_user'
+        );
+        $this->dropForeignKey(
+            'fk-role_user-role_id',
+            'permission_user'
+        );
+
+        $this->dropTable('roles');
+        $this->dropTable('permissions');
+        $this->dropTable('permission_role');
+        $this->dropTable('role_user');
+        $this->dropTable('permission_user');
+    }
+}
---
+++ models/Role.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace vmorozov\entrust\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "roles".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string $display_name
+ * @property string $description
+ *
+ */
+class Role extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'roles';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'string', 'max' => 255],
+            [['name'], 'unique'],
+            [['display_name'], 'string', 'max' => 255],
+            [['description'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'name' => 'Name',
+            'display_name' => 'Display Name',
+            'description' => 'Description',
+        ];
+    }
+}
